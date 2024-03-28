824b67241c6ffc7e070e972b5795f3b5807f0ac3
amsa2002 <amsa.ramanujam@gmail.com>
2024-03-28 14:35:17 +0530

    updated

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './ListProduct.css'
 import cross_icon from '../../assets/cross_icon.png'
-import { Link } from 'react-router-dom'
 
 
 function EditProductModal({ product,image, setImage  , updateProductDetails, saveEditedProduct, closeModal }) {
