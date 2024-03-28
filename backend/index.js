8bada2bfa472d527dd7f4677eb1b52c98cb7dbfb
amsa2002 <amsa.ramanujam@gmail.com>
2024-03-28 13:23:48 +0530

    url updated

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ const Razorpay = require("razorpay")
 const crypto = require("crypto")
 const bodyParser = require('body-parser')
 
+
 app.use(express.json())
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
